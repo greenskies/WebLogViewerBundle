3bc65a0a845e001948f95495c5b204f1b4c3cfdb
Todd Eidson <todd@eidson.info>
2017-11-03 09:35:27 -0400

    Viewer displays view

--- Controller/LogViewerController.php
+++ Controller/LogViewerController.php
@@ -2,14 +2,37 @@
 
 namespace Eidsonator\WebLogViewerBundle\Controller;
 
+use Eidsonator\WebLogViewerBundle\Model\LogView;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class LogViewerController extends Controller
 {
+    /**
+     * @Route("log_viewer", name="log_viewer")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function logViewAction(Request $request)
     {
-        $this->render('@WebLogViewer/logView.html.twig');
+        $log = $request->query->get('log');
+        $delete = filter_var($request->query->get('delete'), FILTER_VALIDATE_BOOLEAN);
+
+        $logDir = $this->container->get('kernel')->getLogDir();
+        $logfile = "$logDir/$log";
+
+        if($delete) {
+            unlink($logfile);
+            return $this->redirectToRoute('eidsonator_weblogviewer_loglist_loglist');
+        }
+
+        if (file_exists($logfile)) {
+            $log = file_get_contents($logfile);
+            $context['log'] = LogView::logToArray($log);
+        } else {
+            $context['noLog'] = true;
+        }
+        return $this->render('@WebLogViewer/logView.html.twig');
     }
 }
