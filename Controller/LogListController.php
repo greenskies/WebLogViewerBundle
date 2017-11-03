8174551da3e07552eba8c9cc210d9eb3a7d7650b
Todd Eidson <todd@eidson.info>
2017-11-03 09:34:08 -0400

    ListController displays view

--- Controller/LogListController.php
+++ Controller/LogListController.php
@@ -2,14 +2,24 @@
 
 namespace Eidsonator\WebLogViewerBundle\Controller;
 
+use Eidsonator\WebLogViewerBundle\Model\LogList;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class LogListController extends Controller
 {
-    public function listAction(Request $request)
+    /**
+     * @Route("logs")
+     */
+    public function logListAction(Request $request)
     {
-        $this->render('@WebLogViewer/listView.html.twig');
+        $kernel = $this->container->get('kernel');
+        $logDir = $kernel->getLogDir();
+        $logs = (new LogList())->getLogList($logDir);
+        $this->render('@WebLogViewer/listView.html.twig', [
+            'logs' => $logs
+        ]);
     }
+
 }
