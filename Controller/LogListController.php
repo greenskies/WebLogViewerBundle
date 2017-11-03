e1ee994f091293e91bb647472ebfcd6cf638f135
Todd Eidson <todd@eidson.info>
2017-11-02 20:29:44 -0400

    Start controllers and views

---
+++ Controller/LogListController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: todd
+ * Date: 02/11/17
+ * Time: 8:21 PM
+ */
+
+namespace Eidsonator\WebLogViewerBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class LogListController extends Controller
+{
+    public function listAction()
+    {
+
+    }
+}
---
+++ Controller/LogViewerController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: todd
+ * Date: 02/11/17
+ * Time: 8:21 PM
+ */
+
+namespace Eidsonator\WebLogViewerBundle\Controller;
+
+
+class LogViewerController
+{
+
+}
